30238b403bcb20ac11b83538892b6fe17c6b89a0
Guilherme Gomes <guiga.gomes10@gmail.com>
2020-07-26 21:36:11 -0300

    Adicionado Theme do Material UI

---
+++ src/theme/palette.js
@@ -0,0 +1,14 @@
+export default {
+    primary: {
+        light: '#46CF40',
+        main: '#79C74C',
+    },
+    secondary: {
+        main: '#525174'
+    },
+    default: {
+        black: '#1D1D1D',
+        white: '#FFFFFF',
+        lightGrey: '#F6F6F6'
+    }
+}
---
+++ src/theme/typography.js
@@ -0,0 +1,7 @@
+export default {
+    fontFamily: 'IBM Plex Mono',
+    h1: {
+        fontSize: 28,
+        fontWeight: '700',
+    }
+}
