1d0edc0085ee05e4a0cf807271931158d9980863
Guilherme Gomes <guiga.gomes10@gmail.com>
2020-07-28 05:05:40 -0300

    Adicionado Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,6 +8,7 @@ import {
 } from "@material-ui/core";
 
 import AccountCircle from "@material-ui/icons/AccountCircle";
+import SearchIcon from '@material-ui/icons/Search';
 
 // Internal Components
 import styles from "./styles";
@@ -17,8 +18,8 @@ const Header = ({ classes }) => {
   return (
     <Container maxWidth={false} className={classes.root}>
       <Container>
-        <Grid container justify="space-between" alignItems="center">
-          <Grid item>
+        <Grid container justify="space-between" spacing={2} alignItems="center">
+          <Grid item md={4}>
             <Grid container spacing={2} alignItems="center">
               <Grid item>
                 <img src={logo} alt="Pokémon Store - Logo" />
@@ -30,17 +31,27 @@ const Header = ({ classes }) => {
               </Grid>
             </Grid>
           </Grid>
-          <Grid item>
+          <Grid item md={5}>
             <TextField
               fullWidth
-              label="Busque aqui seu produto"
+              placeholder="Busque aqui seu produto"
               className={classes.searchBar}
               variant="outlined"
-              InputProps={{ endAdornment: <AccountCircle /> }}
+              size="medium"
+              InputProps={{ endAdornment: <SearchIcon color="primary" /> }}
             />
           </Grid>
-          <Grid item>
-              {/* {Colocar saldo disponiVel} */}
+          <Grid item md={3}>
+            {/* {Colocar saldo disponiVel} */}
+            <Grid container spacing={1} justify="flex-end" alignItems="center">
+              <Grid item>
+                <AccountCircle className={classes.iconBalance} />
+              </Grid>
+              <Grid item>
+                <Typography variant="body1" color="textSecondary" className={classes.balanceTitle}>Saldo Disponível</Typography>
+                <Typography variant="caption" color="textSecondary">R$30,00</Typography>
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
       </Container>
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,26 +1,34 @@
-export default theme => ({
-    root: {
-        backgroundColor: theme.palette.primary.main
+export default (theme) => ({
+  root: {
+    backgroundColor: theme.palette.primary.main,
+  },
+  typeStore: {
+    color: theme.palette.default.white,
+    textTransform: "uppercase",
+    "&::before": {
+      content: '" "',
+      display: "block",
+      height: 2,
+      width: "100%",
+      backgroundColor: "white",
     },
-    typeStore: {
-        color: theme.palette.default.white,
-        textTransform: 'uppercase',
-        '&::before': {
-            content: '" "',
-            display: 'block',
-            height: 2,
-            width: '100%',
-            backgroundColor: 'white',
-        },
-        '&::after': {
-            content: '" "',
-            display: 'block',
-            height: 2,
-            width: '100%',
-            backgroundColor: 'white',
-        },
+    "&::after": {
+      content: '" "',
+      display: "block",
+      height: 2,
+      width: "100%",
+      backgroundColor: "white",
     },
-    searchBar: {
-        backgroundColor: theme.palette.default.white,
-    }
-})
+  },
+  searchBar: {
+    backgroundColor: theme.palette.default.white,
+    borderRadius: 5,
+  },
+  iconBalance: {
+    fontSize: 46,
+    color: theme.palette.default.white,
+  },
+  balanceTitle: {
+    fontWeight: "700",
+  },
+});
