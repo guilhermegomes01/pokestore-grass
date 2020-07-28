92fbda6884899d334578bd1716d8dfdf824353f8
Guilherme Gomes <guiga.gomes10@gmail.com>
2020-07-28 18:02:13 -0300

    Ajustes no Resumo do Carrinho

--- src/views/Cart/components/CustomLayoutSummary.js
+++ src/views/Cart/components/CustomLayoutSummary.js
@@ -1,16 +1,43 @@
 import React from "react";
 import { withStyles, Grid, Typography } from "@material-ui/core";
 
-const styles = (theme) => ({});
+const styles = (theme) => ({
+  subtotal: {
+    fontWeight: '600',
+    fontSize: 18,
+    color: `${theme.palette.default.black}80`
+  },
+  total: {
+    fontWeight: '700',
+    fontSize: 18
+  },
+  balance: {
+    fontWeight: '700',
+    fontSize: 18,
+    color: theme.palette.primary.main
+  }
+});
 
-const LayoutSummary = ({ leftText, rightText }) => {
+const LayoutSummary = ({ name, classes, style, leftText, rightText }) => {
   return (
-    <Grid container justify="space-between">
+    <Grid style={{ ...style }} container justify="space-between">
       <Grid item>
-        <Typography>{leftText}</Typography>
+        {name === "subtotal" ? (
+          <Typography className={classes.subtotal}>{leftText}</Typography>
+        ) : name === 'total' ? (
+          <Typography className={classes.total}>{leftText}</Typography>
+        ) : (
+          <Typography className={classes.balance}>{leftText}</Typography>
+        )}
       </Grid>
       <Grid item>
-        <Typography>{rightText}</Typography>
+      {name === "subtotal" ? (
+          <Typography className={classes.subtotal}>{rightText}</Typography>
+        ) : name === 'total' ? (
+          <Typography className={classes.total}>{rightText}</Typography>
+        ) : (
+          <Typography className={classes.balance}>{rightText}</Typography>
+        )}
       </Grid>
     </Grid>
   );
